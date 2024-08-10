74ffe8cdbf7ff862a188a88392bad5b59a95afda
Mehdi Ali <mehdikhanofficial@gmail.com>
2024-08-10 13:01:56 +0500

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home({searchParams}:SearchParamProps) {
       </section>
 
       <Image
-        src='/assets/images/onboarding-img.png'
+        src='/assets/images/onboarding.png'
         height={1000}
         width={1000}
         alt="patient"
